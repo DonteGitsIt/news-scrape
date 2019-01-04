d3736cf2921c51c4d07074d3dc40ea62dae1911f
Donte Ojeda <donte.ojeda@gmail.com>
2019-01-04 15:22:58 -0500

    file set up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 var mongoose = require("mongoose");
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var express = require("express");
 
@@ -12,7 +12,7 @@ var app = express();
 //scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
-var db = require("./models");
+// var db = require("./models");
 
 app.use(express.static("public"));
 
@@ -25,7 +25,16 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// possibly change
+// var routes = require("./controllers/catsController.js");
 
+// app.use(routes);
+
+//Routes
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
 
 app.listen(PORT, function() {
